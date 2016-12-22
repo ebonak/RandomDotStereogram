c2ac9c6ce89c46eb6d49f234290e16c7affa92c4
Esmail <ebonakdarian@dom.edu>
2016-12-21 21:40:22 -0600

    Adding README.txt file

---
+++ README.txt
@@ -0,0 +1,6 @@
+I plan to implement a simple radom dot stereogram as described in this
+YouTube video
+
+https://www.youtube.com/watch?v=v8O8Em_RPNg
+
+eb  december 2016
--- RandomDotStereogram/Form1.Designer.cs
+++ RandomDotStereogram/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RandomDotStereogram";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
